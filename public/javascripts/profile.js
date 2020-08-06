6c2271a152205322db88ad194f7bbcb3da7b2ebe
Mohammad Arham <arhammusheer@gmail.com>
2020-08-06 22:40:34 +0530

    added profile features

--- public/javascripts/profile.js
+++ public/javascripts/profile.js
@@ -1,9 +1,25 @@
 phoneView = document.getElementById("phone-view");
 phoneEdit = document.getElementById("phone-edit");
 phoneEditButton = document.getElementById("phone-edit-button");
+phonoeEditCancelButton = document.getElementById("cancel-button");
 
 phoneEditButton.onclick = function () {
-  phoneView.hidden = true;
+  $("#phone-view").fadeOut();
+  $("#phone-edit").fadeIn();
+  //phoneView.hidden = true;
   phoneEdit.hidden = false;
 };
+phonoeEditCancelButton.onclick = function(){
+  $("#phone-view").fadeIn();
+  $("#phone-edit").fadeOut();
+  //phoneView.hidden = true;
+  phoneEdit.hidden = true;
+}
 
+$("#user-nickname").keypress(function (e) {
+  var length = this.value.length;
+  if (length > 30) {
+    e.preventDefault();
+    alert("Max limit: 30 characters");
+  }
+});
