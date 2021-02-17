5502a2aa499760b3d40a21ce2164afd1925bc501
arhammusheer <arhammusheer@gmail.com>
2021-02-18 01:44:54 +0530

    patch

--- models/User.js
+++ models/User.js
@@ -3,13 +3,24 @@ var findOrCreate = require("mongoose-findorcreate");
 
 var Schema = mongoose.Schema;
 
+function getRandomColor() {
+  var letters = "0123456789ABCDEF";
+  var color = "#";
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
+
+const generate = require("name-creator");
+
 var UserSchema = new Schema({
   displayName: String,
   firstName: String,
   lastName: String,
   username: String,
-  nickname: String,
-  color: String,
+  nickname: { type: String, default: generate().dashed },
+  color: { type: String, default: getRandomColor() },
   googleId: String,
   email: String,
   pictureUrl: String,
